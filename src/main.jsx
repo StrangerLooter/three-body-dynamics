362d2fbbbae459b4c99d4b7470781c4cfa541065
Ram Vishwakarma <rv324102@gmail.com>
2026-08-16 00:36:24 +0530

    main.jsx: import from Simulator.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ThreeBodySimulator from './Simulator.jsx';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div style={{ width: '100vw', height: '100vh' }}>
