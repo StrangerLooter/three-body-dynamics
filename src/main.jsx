6b05a25794868f663d23ddf783a97f1ad71ff84f
Ram Vishwakarma <rv324102@gmail.com>
2026-08-15 19:09:53 +0530

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ThreeBodySimulator from './Simulator.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ThreeBodySimulator />
+    </div>
+  </React.StrictMode>
+);
